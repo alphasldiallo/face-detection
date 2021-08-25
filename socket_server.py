9376fb3434c38f2a66902408e5b862ddd8c45c6b
Alpha Diallo <alpha.diallo@unil.ch>
2021-08-25 17:42:08 +0200

    Socket configured

--- socket_server.py
+++ socket_server.py
@@ -1,28 +1,59 @@
-import socketserver
+import socket
+from threading import Thread
 
 
-class MyTCPHandler(socketserver.BaseRequestHandler):
-    """
-    The request handler class for our server.
-    It is instantiated once per connection to the server, and must
-    override the handle() method to implement communication to the
-    client.
-    """
+class Server:
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server.bind((self.host, self.port))
+        self.server.listen(5)
 
-    def handle(self):
-        # self.request is the TCP socket connected to the client
-        self.data = self.request.recv(1024).decode("utf-8").strip()
-        print(self.data)
+    def listen_for_clients(self):
+        print('Listening...')
+        while True:
+            client, addr = self.server.accept()
+            print(
+                'Accepted Connection from: ' + str(addr[0]) + ':' + str(addr[1])
+            )
+            Thread(target=self.handle_client, args=(client, addr)).start()
+
+    def handle_client(self, client_socket, address):
+        size = 1024
+        while True:
+            try:
+                data = client_socket.recv(size)
+                if 'q^' in data.decode():
+                    print('Received request for exit from: ' + str(
+                        address[0]) + ':' + str(address[1]))
+                    break
+
+                else:
+                    # send getting after receiving from client
+                    client_socket.sendall('Welcome to server'.encode())
+
+                    print('Received: ' + data.decode() + ' from: ' + str(
+                        address[0]) + ':' + str(address[1]))
+
+            except socket.error:
+                client_socket.close()
+                return False
+
+        client_socket.sendall(
+            'Received request for exit. Deleted from server threads'.encode()
+        )
+
+        # send quit message to client too
+        client_socket.sendall(
+            'q^'.encode()
+        )
+        client_socket.close()
 
 
 if __name__ == "__main__":
-    HOST, PORT = "127.0.0.1", 1223
-
-    with socketserver.TCPServer((HOST, PORT), MyTCPHandler) as server:
-        # Activate the server; this will keep running until you
-        # interrupt the program with Ctrl-C
-        try:
-            print("Starting the server on {}...".format(HOST))
-            server.serve_forever()
-        except KeyboardInterrupt:
-            server.shutdown()
+    host = '172.20.10.2'
+    port = 1223
+    main = Server(host, port)
+    # start listening for clients
+    main.listen_for_clients()
